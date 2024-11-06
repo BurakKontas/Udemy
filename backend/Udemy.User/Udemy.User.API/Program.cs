52ac543114d8f29e154597fa37e6c5c55ed78d37
Arda Burak Kontaş <aburakkontas@hotmail.com>
2024-11-06 15:44:25 +0300

    Initialize web application with default services
    Added a new using directive for the Udemy.ServiceDefaults namespace.
    Created a WebApplicationBuilder instance using WebApplication.CreateBuilder(args).
    Added the ProblemDetails service to the service collection using builder.Services.AddProblemDetails().
    Called the AddServiceDefaults extension method on the builder to add default services.
    Built the application using builder.Build().
    Configured the HTTP request pipeline to map default endpoints using app.MapDefaultEndpoints().
    Called app.Run() to start the application.

--- backend/Udemy.User/Udemy.User.API/Program.cs
+++ backend/Udemy.User/Udemy.User.API/Program.cs
@@ -1,7 +1,14 @@
+using Udemy.ServiceDefaults;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddProblemDetails();
+
+builder.AddServiceDefaults();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapDefaultEndpoints();
 
 app.Run();
